cc5f37808799ec3498f3098bea67d1587aa51388
Henrique de Souza Carvalho <35101920+henriquedesouzacarvalho@users.noreply.github.com>
2019-10-13 15:53:47 -0300

    Update index.js
    Alteração do nome do site

--- frontend/src/pages/Index/index.js
+++ frontend/src/pages/Index/index.js
@@ -4,7 +4,7 @@ import logo from '../../assets/reclamacao.png'
 export default function Index() {
     return (
         <>
-            <h1 className="text-center home">Bem vindo ao Reclamador</h1>
+            <h1 className="text-center home">Bem vindo ao Reclame aí</h1>
             <div className="logo">
                 <img src={logo} alt="Logo"></img>
             </div>
